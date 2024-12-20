d033a7b50e97c715963f6cf0332d142e5d2bb1fb
Nurgeldi Tuvakov <77510714+nurgelli@users.noreply.github.com>
2024-12-20 11:31:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const navLinks = document.querySelectorAll('.navigation a');
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
